926524bb512924566a23d32f83dcf9020847110e
Ayyub <ayyubbijapure07@gmail.com>
2026-01-04 11:52:04 +0530

    Wake word models download

---
+++ Research/wake_word/models_download.py
@@ -0,0 +1,5 @@
+import openwakeword
+
+# This will download melspectrogram.onnx and other defaults to your venv folder
+openwakeword.utils.download_models() 
+print("Models downloaded successfully.")
